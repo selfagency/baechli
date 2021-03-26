ad2f46c60888a48504079eaab505050746f8feb5
Sofiene Khiari <64355269+sofienekhiari@users.noreply.github.com>
2021-03-26 21:18:26 +0100

    Execute all commands in the right order

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 // Import relevant modules
 import {Command} from 'commander';
+const {spawn} = require('child_process');
+const process = require('process');
 
 // Create a new program handler
 const program = new Command();
@@ -14,10 +16,34 @@ program.requiredOption('-c, --commands <cmd...>', 'Commands to run');
 program.parse(process.argv);
 const options = program.opts();
 
-// Process and log each command
+// Process and execute each command
 options.commands.forEach(function(cmd: string) {
+  // Process the command
   const cmdArray = cmd.split(' ');
   const cmdProgram: string = cmdArray[0];
   const cmdArgs: string[] = cmdArray.slice(1);
-  console.log(`${cmdProgram} | ${cmdArgs}`);
+
+  // Execute the command
+  // process.stdout.write(`Info: Running "${cmd}" ...`);
+  const childProcess = spawn(cmdProgram, cmdArgs);
+  // process.stdout.write('\r\x1b[K');
+  // process.stdout.write(`Info: Running "${cmd}" ... done.\n`);
+
+  // Handle execution errors
+  childProcess.on('error', function() {
+    console.log('Error executing the command "' + cmd + '"');
+  });
+
+  // Handle the exit conditions
+  childProcess.on('exit', function(exitCode: number, exitSignal: string) {
+    if (exitCode == 0) {
+      childProcess.stdout.on('data', (data: string) => {
+        console.log(`${data}`);
+      });
+    } else {
+      childProcess.stderr.on('data', (data: string) => {
+        console.error(`${data}`);
+      });
+    }
+  });
 });

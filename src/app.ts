fad872b3af8fe39ba7dc596d6e55f53059f9950b
Sofiene Khiari <64355269+sofienekhiari@users.noreply.github.com>
2021-03-29 21:54:18 +0200

    0.1.4

--- src/app.ts
+++ src/app.ts
@@ -73,7 +73,7 @@ function run() {
   // Create a new program handler.
   const program = new Command();
   // Specify the current version.
-  program.version('0.1.3');
+  program.version('0.1.4');
   // Specify the configuration for the "commands" flag.
   program.requiredOption('-c, --commands <cmd...>', 'Commands to run');
   // Parse the command line arguments.
